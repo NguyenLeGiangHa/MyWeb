3a3b592a6e1383349539fd646fb8095ae0bd5e88
NguyenLeGiangHa <giangha09010203@gmail.com>
2023-10-10 09:13:28 +0700

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,104 +1,104 @@
-// // icons
-// import {
-//   FaHtml5,
-//   FaCss3,
-//   FaJs,
-//   FaReact,
-//   FaWordpress,
-//   FaFigma,
-// } from "react-icons/fa";
-//
-// import {
-//   SiNextdotjs,
-//   SiFramer,
-//   SiAdobexd,
-//   SiAdobephotoshop,
-// } from "react-icons/si";
-//
-// import React, { useState} from "react";
-// //  data
-// export const aboutData = [
-//   {
-//     title: 'skills',
-//     info: [
-//       {
-//         title: 'Web Development',
-//         icons: [
-//           <FaHtml5 />,
-//           <FaCss3 />,
-//           <FaJs />,
-//           <FaReact />,
-//           <SiNextdotjs />,
-//           <SiFramer />,
-//           <FaWordpress />,
-//         ],
-//       },
-//       {
-//         title: 'UI/UX Design',
-//         icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
-//       },
-//     ],
-//   },
-//   {
-//     title: 'awards',
-//     info: [
-//       {
-//         title: 'Webby Awards - Honoree',
-//         stage: '2011 - 2012',
-//       },
-//       {
-//         title: 'Adobe Design Achievement Awards - Finalist',
-//         stage: '2009 - 2010',
-//       },
-//     ],
-//   },
-//   {
-//     title: 'experience',
-//     info: [
-//       {
-//         title: 'UX/UI Designer - XYZ Company',
-//         stage: '2012 - 2023',
-//       },
-//       {
-//         title: 'Web Developer - ABC Agency',
-//         stage: '2010 - 2012',
-//       },
-//       {
-//         title: 'Intern - DEF Corporation',
-//         stage: '2008 - 2010',
-//       },
-//     ],
-//   },
-//   {
-//     title: 'credentials',
-//     info: [
-//       {
-//         title: 'Web Development - ABC University, LA, CA',
-//         stage: '2011',
-//       },
-//       {
-//         title: 'Computer Science Diploma - AV Technical Institute',
-//         stage: '2009',
-//       },
-//       {
-//         title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-//         stage: '2006',
-//       },
-//     ],
-//   },
-// ];
-//
-// import Avatar  from "../../components/Avatar";
-// import Circles from "../../components/Circles";
-//
-// //framer motion
-// import {motion} from 'framer-motion'
-// import  {fadeIn} from "../../variants";
-//
-// //counters
-// import CountUp from "react-countup";
-//
-// const About = () => {
+// icons
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaWordpress,
+  FaFigma,
+} from "react-icons/fa";
+
+import {
+  SiNextdotjs,
+  SiFramer,
+  SiAdobexd,
+  SiAdobephotoshop,
+} from "react-icons/si";
+
+import React, { useState} from "react";
+//  data
+export const aboutData = [
+  {
+    title: 'skills',
+    info: [
+      {
+        title: 'Web Development',
+        icons: [
+          <FaHtml5 />,
+          <FaCss3 />,
+          <FaJs />,
+          <FaReact />,
+          <SiNextdotjs />,
+          <SiFramer />,
+          <FaWordpress />,
+        ],
+      },
+      {
+        title: 'UI/UX Design',
+        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+      },
+    ],
+  },
+  {
+    title: 'awards',
+    info: [
+      {
+        title: 'Webby Awards - Honoree',
+        stage: '2011 - 2012',
+      },
+      {
+        title: 'Adobe Design Achievement Awards - Finalist',
+        stage: '2009 - 2010',
+      },
+    ],
+  },
+  {
+    title: 'experience',
+    info: [
+      {
+        title: 'UX/UI Designer - XYZ Company',
+        stage: '2012 - 2023',
+      },
+      {
+        title: 'Web Developer - ABC Agency',
+        stage: '2010 - 2012',
+      },
+      {
+        title: 'Intern - DEF Corporation',
+        stage: '2008 - 2010',
+      },
+    ],
+  },
+  {
+    title: 'credentials',
+    info: [
+      {
+        title: 'Web Development - ABC University, LA, CA',
+        stage: '2011',
+      },
+      {
+        title: 'Computer Science Diploma - AV Technical Institute',
+        stage: '2009',
+      },
+      {
+        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
+        stage: '2006',
+      },
+    ],
+  },
+];
+
+import Avatar  from "../../components/Avatar";
+import Circles from "../../components/Circles";
+
+//framer motion
+import {motion} from 'framer-motion'
+import  {fadeIn} from "../../variants";
+
+//counters
+import CountUp from "react-countup";
+
+const About = () => {
 //   const [index, setIndex] = useState(0);
 //   console.log(index);
 //   return (
@@ -236,6 +236,6 @@
 //       </div>
 //     </div>
 //   );
-// };
-//
-// export default About;
+};
+
+export default About;
